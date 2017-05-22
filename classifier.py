4ae75d31ad9c38a978ea757df5c4985e3362c905
zjxiaobai <897665728@qq.com>
2017-05-22 17:08:34 +0800

    使用交叉验证评估模型的好坏

--- classifier.py
+++ classifier.py
@@ -6,45 +6,46 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import r2_score
 from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import KFold
 
 df = pd.read_csv('data.csv')
 
+# ===============================
+
 prices = df['lable']
 features = df.drop('lable', axis = 1)
-X_train, X_test, y_train, y_test = train_test_split(features,prices,test_size=0.2,random_state=42)
 
-if False:
-	clf = GaussianNB()
-	clf.fit(X_train, y_train)
+features = np.array(features)
+prices = np.array(prices)
 
-	y_predict = clf.predict(X_test)
+kf = KFold(653, n_folds=10)
 
 
-if False:
+for train_index, test_index in kf:
+	# print("TRAIN:", train_index, "TEST:", test_index)
+
+	X_train, X_test = features[train_index], features[test_index]
+	y_train, y_test = prices[train_index], prices[test_index]
+
+	# X_train = [ features[ii] for ii in train_index ] 
+	# X_test  = [ features[ii] for ii in test_index ] 
+
+	# y_train = [ prices[ii] for ii in train_index ] 
+	# y_test  = [ prices[ii] for ii in test_index ] 
+
 	clf = svm.SVC()
 	clf.fit(X_train, y_train) 
 
 	y_predict = clf.predict(X_test)
 
-if True:
-	clf = tree.DecisionTreeClassifier()
-	clf.fit(X_train, y_train)
+	acc = accuracy_score(y_predict, y_test)
 
-	y_predict = clf.predict(X_test)
+	score = r2_score(y_predict, y_test)
 
+	print("accuracy:")
+	print( acc )
 
+	print("r2_score:")
+	print( score )
 
-acc = accuracy_score(y_predict, y_test)
-
-score = r2_score(y_predict, y_test)
-
-print("accuracy:")
-print( acc )
-
-print("r2_score:")
-print( score )
-
-
-
-
-
+	print("")
