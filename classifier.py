9e08f273985ef87ec261253df01a959bec5925fd
zjxiaobai <897665728@qq.com>
2017-05-22 17:44:53 +0800

    补齐三种分类器

--- classifier.py
+++ classifier.py
@@ -22,21 +22,29 @@ kf = KFold(653, n_folds=10)
 
 
 for train_index, test_index in kf:
-	# print("TRAIN:", train_index, "TEST:", test_index)
 
 	X_train, X_test = features[train_index], features[test_index]
-	y_train, y_test = prices[train_index], prices[test_index]
+	y_train, y_test = prices[train_index], prices[test_index] 
 
-	# X_train = [ features[ii] for ii in train_index ] 
-	# X_test  = [ features[ii] for ii in test_index ] 
+	if False:
+		clf = GaussianNB()
+		clf.fit(X_train, y_train)
 
-	# y_train = [ prices[ii] for ii in train_index ] 
-	# y_test  = [ prices[ii] for ii in test_index ] 
+		y_predict = clf.predict(X_test)
 
-	clf = svm.SVC()
-	clf.fit(X_train, y_train) 
 
-	y_predict = clf.predict(X_test)
+	if False:
+		clf = svm.SVC()
+		clf.fit(X_train, y_train) 
+
+		y_predict = clf.predict(X_test)
+
+	if True:
+		clf = tree.DecisionTreeClassifier()
+		clf.fit(X_train, y_train)
+
+		y_predict = clf.predict(X_test)
+		
 
 	acc = accuracy_score(y_predict, y_test)
 
@@ -49,3 +57,43 @@ for train_index, test_index in kf:
 	print( score )
 
 	print("")
+
+
+
+
+# ===============================
+
+# prices = df['lable']
+# features = df.drop('lable', axis = 1)
+# X_train, X_test, y_train, y_test = train_test_split(features,prices,test_size=0.2,random_state=42)
+
+# if False:
+# 	clf = GaussianNB()
+# 	clf.fit(X_train, y_train)
+
+# 	y_predict = clf.predict(X_test)
+
+
+# if False:
+# 	clf = svm.SVC()
+# 	clf.fit(X_train, y_train) 
+
+# 	y_predict = clf.predict(X_test)
+
+# if True:
+# 	clf = tree.DecisionTreeClassifier()
+# 	clf.fit(X_train, y_train)
+
+# 	y_predict = clf.predict(X_test)
+
+
+
+# acc = accuracy_score(y_predict, y_test)
+
+# score = r2_score(y_predict, y_test)
+
+# print("accuracy:")
+# print( acc )
+
+# print("r2_score:")
+# print( score )
